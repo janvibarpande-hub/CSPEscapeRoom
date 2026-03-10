6bf1e9748c4f867bef7243d6f2b83c61d201e4dd
garrattch <garrattch@wgc.school.nz>
2026-03-10 14:22:19 +1300

    Add files via upload

--- room2_task1.py
+++ room2_task1.py
@@ -1,17 +1,31 @@
-print("Room 2")
+print("Room 2, task 1")
+#setting the random weight
 import random
-weight = random.uniform(10, 20)
+rannum = random.uniform(10, 20)
+weight = round(rannum, 1)
+#guess count
+guesses = 3
+#inventory options with weight (not needed)
 basket_list = [["flower", 1.1], ["cookie", 1.5], ["Muffin", 2]]
-print("There are 3 items in the basket: flowers, cookies, and muffins.")
-print("The flowers weigh 1.1")
-print("The cookies weigh 1.5")
-print("The muffins weigh 2")
-print("Calculate the number of each item that is needed for the basket to weigh {:.1dp}.".format(weight))
-flower = int(input("How many flowers are needed? "))
-cookie = int(input("How many cookies are needed? "))
-muffin = int(input("How many muffins are needed? "))
-num_list = []
-num_list.append(flower)
-num_list.append(cookie)
-num_list.append(muffin)
-print(num_list)
+#instructions
+input("There are 3 items in the basket: flowers, cookies, and muffins.")
+input("A flower weighs 1.1")
+input("A cookie weighs 1.5")
+input("A muffins weighs 2")
+input("You can have 3 total guesses")
+input(f"Calculate the number of each item that is needed for the basket to weigh {weight}.")
+#inventory guess input
+while guesses > 0:
+    flower = int(input("How many flowers are needed? ")) * 1.1
+    cookie = int(input("How many cookies are needed? ")) * 1.5
+    muffin = int(input("How many muffins are needed? ")) * 2
+#guess = weight check
+    total = flower + cookie + muffin
+    if total != weight:
+        guesses -= 1
+        input(f"That is incorrect, you have {guesses} attempts left.")
+    else:
+        print("That is correct!")
+    break
+if guesses <= 0:
+    print("You lost! Game Over")
