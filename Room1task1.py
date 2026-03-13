f604b2c916bb3041cfedd2bbd36dcfac9a8fe215
alagappanga2-maker <alagappanga2@wgc.school.nz>
2026-03-13 15:08:41 +1300

    Add files via upload
    devlopment 13/3

--- Room1task1.py
+++ Room1task1.py
@@ -24,26 +24,32 @@ print(og_graph)
 import random
 import time
 #generate the correct coordinate
-letters = "ABCDEF"
-rand_index = random.randint(0, 5)
+letters = "ABCDE"
+rand_index = random.randint(0, 4)
 rand_x = letters[rand_index]
 rand_y = random.randint(1, 6)
 #full coordinate
 coordinate = (f"{rand_x}{rand_y}")
 print(coordinate)
+#counter of lives and keys
+game_won = False
+lives = 7
+key = 0
 
-#counter of lives
-lives = 10
+print("--- The Wolf is Hiding! ---")
+
 #asking user to guess
-guess = input("Enter a coordinate (e.g., A1): ").upper()
-if guess == coordinate:
-    print("Correct guess")
-    time.sleep(1.0)
-    print("Great job player, I guess you are smarter that i thought")
-    time.sleep(1.0)
-    print("you recieved your first key")
-    time.sleep(1.0)
-    print("""                                                                  
+while lives > 0 and not game_won:
+    print(f"\nLives remaining: {lives}")
+    guess = input("Enter a coordinate (e.g., A1): ").upper()
+    if guess == coordinate:
+        print("Correct guess")
+        time.sleep(1.0)
+        print("Great job player, I guess you are smarter that i thought")
+        time.sleep(1.0)
+        print("you recieved your first key")
+        time.sleep(1.0)
+        print("""                                                                  
                ,-----.                                                          
              ,' ,-+-. `.                                                        
             /  / | | \  \                                                       
@@ -61,8 +67,17 @@ if guess == coordinate:
                 |+_|_|_|                                                        
                 \ |                                                             
                  `|       """)
-elif guess != 
-                                                                                
-                                                                                
-                                                                                
-                                                                                
+        game_won = True
+    else:
+        lives -= 1
+        
+        if lives == 0:
+            print("Game over... The wolf got away.")
+        if lives == 3:
+            print("I see you're struggling a bit there.")
+            print(f"HINT: The wolf is in column {rand_x}")
+        else:
+            print("Wrong answer player! Try again.")
+if game_won == True:
+    print("""Congradulations player, You have sucsessfully found wolf and recieved a
+      key but I wonder what else he has in plan for you""")
