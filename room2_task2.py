9e287f8e33bca24ae9d247c5a91eaabb4bee4775
garrattch <garrattch@wgc.school.nz>
2026-03-18 09:22:16 +1300

    Add files via upload
    17 march 2026

--- room2_task2.py
+++ room2_task2.py
@@ -1,4 +1,5 @@
 import random
+import time
 phrase_list = ["grandmother what big eyes you have",
                "all the better to see you with my dear",
                "grandmother what big ears you have",
@@ -13,14 +14,20 @@ string_list = list(phrase)
 random.shuffle(string_list)
 new_phrase = "".join(string_list)
 
-guess = 5
-input("You have arrived at Granny's house but she has a lock on her door.")
-input("To unlock the door you need to unscramble the phrase in 5 or less guesses")
-input("The phrase is: ")
-input(new_phrase)
-unscrambled = input("Please enter the uncrambled phrase: ")
-if uncrambled == phrase:
-    print("That is correct")
-else:
-    guesses -= 1
-    print(f"That is incorrect, you have {guesses} left")
+guess = 8
+print("You have arrived at Granny's house but she has a lock on her door.")
+time.sleep(4)
+print("To unlock the door you need to unscramble the phrase in 6 or less guesses")
+time.sleep(4)
+print("The phrase is: ")
+time.sleep(4)
+print(new_phrase)
+time.sleep(4)
+while guess > 0:
+    unscrambled = input("Please enter the uncrambled phrase: ")
+    if unscrambled == phrase:
+        print("That is correct")
+        break
+    else:
+        guess -= 1
+        print(f"That is incorrect, you have {guess} left")
